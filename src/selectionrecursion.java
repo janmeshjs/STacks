886141159607fbf5394cf813366789da02872940
janmesh Singh <Janmesh.js@gmail.com>
2023-05-06 12:47:41 +0530

    update

--- src/selectionrecursion.java
+++ src/selectionrecursion.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-
+//recursive aproach fpr selection sort
 public class selectionrecursion {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
