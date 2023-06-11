75e30ce9705fe1d0b9b52ce7f3639f74ad178370
janmesh Singh <Janmesh.js@gmail.com>
2023-06-11 22:50:53 +0530

    update

--- src/recursion/permutation/permutationrec.java
+++ src/recursion/permutation/permutationrec.java
@@ -1,13 +1,16 @@
 package recursion.permutation;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 // program to find the permutation of strings 
 
 
 public class permutationrec {
     public static void main(String[] args) {
-        permutation("", "aac");
+
+       ArrayList<String> res= perm("", "abc");
+       System.out.println(res );
     }
 
     public static void permutation(String p, String up){
@@ -22,5 +25,27 @@ public class permutationrec {
 
             permutation(first+ch+second, up.substring(1));
         }
+    }
+
+    //returning in list 
+
+    static ArrayList<String> perm(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch= up.charAt(0);
+
+        ArrayList<String> lis = new ArrayList<>();
+
+        for(int i=0; i<=p.length(); i++){
+            String first = p.substring(0, i);
+            String second = p.substring(i, p.length());
+
+            lis.addAll(perm(first+ch+second, up.substring(1)));
+        }
+        return lis;
+
     }
 }
