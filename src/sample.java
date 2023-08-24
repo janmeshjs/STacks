96f3d0af496a1a0c245f06da1b1084aa98718b4d
janmesh Singh <Janmesh.js@gmail.com>
2023-08-24 10:13:31 +0530

    update

--- src/sample.java
+++ src/sample.java
@@ -1,16 +1,27 @@
 public class sample {
+//*************************************** Accenture test **************
     public static void main(String[] args) {
-        String str = "bcad";
-        System.out.println(sol(str));
+        String s = "xgqvzgainqpnotc";
+        String t = "auqixjp";
+        int[] arr = {3, 1, 2, 4};
+        System.out.println(cal(arr));
     }
 
-    public static String sol(String str){
-        if(str.isEmpty()) return ""; 
+    public static int cal(int[] arr){
+        int c=0;
+        for(int i =0; i<arr.length; i++){
+            int maxi =Integer.MIN_VALUE;
+            int mini = Integer.MAX_VALUE;
+            for(int j =i; j<arr.length; j++){
+                maxi = Math.max(maxi, arr[j]);
+                mini = Math.min(mini, arr[j]);
 
-        if(str.charAt(0)=='a'){
-            return sol(str.substring(1));
-        }else{
-            return str.charAt(0)+ sol(str.substring(1));
+                if((maxi+mini)%(j-i+1)==0)c++;
+            }
         }
+        return c;
     }
 }
+
+
+
